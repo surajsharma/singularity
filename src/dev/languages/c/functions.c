34dbb09649aac0a43d55150c1c5a3a025486a8b4
surajsharma <surajsharma@pm.me>
2024-02-06 19:08:09 +0530

    06.02.24

---
+++ src/dev/languages/c/functions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int display(nums) // no implicit declarations
+int *nums;        // parameter types
+                  // here an array thus pointer notation but nums[10] works
+{
+    int i;
+    for (i = 0; i < 10; i++)
+        printf("%d\n", nums[i]);
+
+    return 0;
+}
+
+int main()
+{
+    int nums[10], i; // cannot have simple dynamic array
+    for (i = 0; i < 10; i++)
+        nums[i] = i;
+    display(nums);
+    return 0;
+}
