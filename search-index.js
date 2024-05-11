37bb8150030207fa6d2ca6a5061cae0f502af211
surajsharma <suraj@evenzero.in>
2024-05-12 02:19:42 +0530

    12.05.24, indexed

--- search-index.js
+++ search-index.js
@@ -3,6 +3,10 @@ const path = require("path");
 const lunr = require("lunr");
 
 const directoryPath = process.argv[2];
+const excludedDirs = ["target", ".", ".ipynb_checkpoints", ".out"];
+const excludedFiles = [".DS_Store", "index.md"];
+const excludedExts = [".png", ".jpg", ".gif", ".lock"];
+
 
 if (!directoryPath) {
   console.log("Please specify the directory path as a command line parameter.");
@@ -27,49 +31,56 @@ function readFileAsString(filePath, encoding = 'utf8') {
 }
 
 async function createSearchIndex(dirPath) {
-  const excludedDirs = ["target", ".", ".ipynb_checkpoints", ".out"];
-  const excludedFiles = [".DS_Store", "index.md"];
-  const excludedExts = [".png", ".jpg", ".gif", ".lock"];
-
-  let items;
 
   try {
-    if (fs.lstatSync(dirPath).isDirectory()) {
-      items = fs.readdirSync(dirPath);
-      var idx = lunr(function () {
-        this.ref('id');
-        this.field('itemPath');
-        this.field('item');
-        this.field('content');
+    const stack = [dirPath];
+    items = fs.readdirSync(dirPath);
 
-        items.forEach(async (item) => {
-          const itemPath = path.join(dirPath, item);
-          const itemStats = fs.lstatSync(itemPath);
+    var idx = lunr(function () {
+      this.ref('id');
+      this.field('itemPath');
+      this.field('item');
+      this.field('content');
+
+      while (stack.length > 0) {
+        const currentPath = stack.pop();
+        const id = generateUniqueId();
+        const contents = fs.readdirSync(currentPath);
+        const item = path.basename(currentPath);
+        console.log({ id, itemPath: currentPath, item, content: '' });
+        this.add({ id, itemPath: currentPath, item, content: '' });
+
+        contents.forEach(async itemName => {
+          const itemPath = path.join(currentPath, itemName);
+          const stats = fs.statSync(itemPath);
           const id = generateUniqueId();
-          if (itemStats.isDirectory()) {
-            if (!excludedDirs.includes(item)) {
-              createSearchIndex(itemPath);
-              this.add({ id, itemPath, item, content: '' });
 
+          if (stats.isDirectory()) {
+            if (!excludedDirs.includes(item)) {
+              stack.push(itemPath);
             }
           } else {
+            const item = path.basename(itemPath);
             if (!excludedFiles.includes(item)) {
               if (!excludedExts.includes(path.extname(item))) {
                 const content = await readFileAsString(itemPath);
                 this.add({ id, itemPath, item, content });
+                console.log({ id, itemPath, item, content });
               }
             }
           }
         });
-      }, this);
+      }
+    }, this);
 
-      fs.appendFileSync(`search.json`, JSON.stringify(idx), function (err) {
-        if (err) throw err;
-      });
-    }
+    fs.appendFileSync(`search.json`, JSON.stringify(idx), function (err) {
+      if (err) throw err;
+    });
   } catch (err) {
     console.error(`Error processing ${dirPath}:`, err);
     process.exitCode = 1;
   }
 }
+
+
 createSearchIndex(directoryPath);
