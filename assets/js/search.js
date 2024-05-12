d3b76a13d0beaf8e3943d0aaa5d5875dd9dc8b01
surajsharma <suraj@evenzero.in>
2024-05-12 23:05:14 +0530

    feat: search frontend

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,8 +6,20 @@ const SRC = baseUrl + 'src-search.json';
 const ARCHIVES = baseUrl + 'archives-search.json';
 
 let index = null;
+let srcIndexData = null;
+let archIndexData = null;
+
+// search modifiers
+let archives = false;
+let fuzzy = false;
+let exact = false;
+let all = false;
 
 const searchInput = document.getElementById('search-input');
+const searchArchives = document.getElementById('search-archives');
+const searchTitles = document.getElementById('search-titles');
+const searchFuzzy = document.getElementById('search-fuzzy');
+const searchExact = document.getElementById('search-exact');
 const searchStatus = document.getElementById('search-status');
 const searchResults = document.getElementById('search-results-container');
 
@@ -23,11 +35,61 @@ function debouncedSearch(searchTerm) {
     const search = index.search(searchTerm);
     console.log('Debounced search:', searchTerm, search);
 
+    displaySearch(search, searchTerm);
+}
+
+function displaySearch(search, searchTerm) {
     searchStatus.innerText = `Found ${search.length} results for ${searchTerm}`;
 
     search.forEach(item => {
         const searchResultDiv = document.createElement('div');
-        searchResultDiv.textContent = item.ref;
+        searchResultDiv.id = `${archives ? 'search-result-arch' : 'search-result'}`;
+
+        const searchResultTitleTop = document.createElement('div');
+        searchResultTitleTop.id = 'search-result-title-top';
+
+        const searchTitleLink = document.createElement('a');
+        searchTitleLink.id = "search-result-title";
+        searchTitleLink.href = `/singularity/${item.ref.replace('.md', '.html')}`;
+
+        const title = item.ref.match(/\/([^/]*)$/)[0]
+            .replace('/', '')
+            .replace('.md', '');
+
+        searchTitleLink.textContent = title.length > 40 ?
+            title.substring(0, 40 - '...'.length) + '...' :
+            title;
+
+        const searchResultScore = document.createElement('div');
+        searchResultScore.id = "search-result-score";
+        searchResultScore.textContent = `⭐ ${item.score}`;
+
+        const searchTitleLinkNewTab = document.createElement('a');
+        searchTitleLinkNewTab.id = "search-result-title-newtab";
+        searchTitleLinkNewTab.href = `/singularity/${item.ref.replace('.md', '.html')}`
+        searchTitleLinkNewTab.textContent = "↗️";
+        searchTitleLinkNewTab.target = '_blank';
+
+        const searchTitleLinkRaw = document.createElement('a');
+        searchTitleLinkRaw.id = "search-result-title-newtab";
+        searchTitleLinkRaw.href = `/singularity/${item.ref}`
+        searchTitleLinkRaw.textContent = "📄";
+        searchTitleLinkRaw.target = '_blank';
+
+        searchResultScore.appendChild(searchTitleLinkNewTab);
+        searchResultScore.appendChild(searchTitleLinkRaw);
+
+        const searchResultLoc = document.createElement('pre');
+        searchResultLoc.id = "search-result-loc";
+        searchResultLoc.textContent = item.ref;
+
+        searchResultTitleTop.appendChild(searchTitleLink);
+        searchResultTitleTop.appendChild(searchResultScore);
+
+
+        searchResultDiv.appendChild(searchResultTitleTop);
+        searchResultDiv.appendChild(searchResultLoc);
+
         searchResults.appendChild(searchResultDiv);
     });
 }
@@ -49,21 +111,59 @@ function loadSearchIndex(data) {
     searchStatus.innerText = '';
 }
 
+function handleToggle(event) {
+    const target = event.target;
+    console.log(`${target.id} was toggled (checked: ${target.checked})`);
+
+    switch (`${target.id}`) {
+        case 'search-archives':
+            archives = target.checked;
+            searchInput.value = '';
+            searchResults.innerHTML = '';
+            if (archives) {
+                loadSearchIndex(archIndexData);
+            }
+            if (!archives) {
+                loadSearchIndex(srcIndexData);
+            }
+            break;
+        case 'search-fuzzy':
+            fuzzy = target.checked;
+            break;
+        case 'search-exact':
+            exact = target.checked;
+            break
+        case 'search-titles':
+            titles = target.checked;
+            break;
+        default:
+            break;
+    }
+}
+
 async function initSearch() {
     if (typeof lunr == 'undefined') {
         return;
     } else {
         // TODO: check local storage, otherwise fetch remote and update local
 
-        let srcIndexData = await getSearchIndexRemote(SRC);
-        let archIndexData = await getSearchIndexRemote(ARCHIVES);
+        srcIndexData = await getSearchIndexRemote(SRC);
+        archIndexData = await getSearchIndexRemote(ARCHIVES);
 
         // load main index
         loadSearchIndex(srcIndexData);
 
+        //event-listeners
+        searchArchives.addEventListener('change', handleToggle);
+        searchTitles.addEventListener('change', handleToggle);
+        searchFuzzy.addEventListener('change', handleToggle);
+        searchExact.addEventListener('change', handleToggle);
+
         // Debounce implementation with timeout
         let timeout;
         if (searchInput) {
+            searchInput.focus();
+            searchInput.value = '';
             searchInput.addEventListener('input', () => {
                 clearTimeout(timeout);
                 timeout = setTimeout(() => {
