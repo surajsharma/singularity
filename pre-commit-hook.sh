244bdd09402088df1923633fc13c548579647bdb
surajsharma <suraj@evenzero.in>
2024-05-12 20:47:00 +0530

    12.05.24, indexed

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -6,10 +6,9 @@ node assets/js/search-index.js src
 node assets/js/search-index.js archives
 echo 
 echo ✅ search indices created
-echo
 echo ⚙️ rebuilding file indexes...
 find . -name "index.md" -type f -delete
 node assets/js/files-index.js archives
 node assets/js/files-index.js src
-echo
 echo ✅ nav indices created!
+echo
