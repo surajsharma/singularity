450dd895237ffaa93c6dbdab5b6ce4161520babd
surajsharma <suraj@evenzero.in>
2024-06-14 01:41:49 +0530

    fix: indexing script precedence

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo ⚙️ building search index...
-rm assets/search/src-search.json
-rm assets/search/archives-search.json
-node assets/js/search-index.js src
-node assets/js/search-index.js archives
-echo 
 echo ✅ search indices created
 echo ⚙️ rebuilding file indexes...
 find . -name "index.md" -type f -delete
@@ -12,6 +6,12 @@ node assets/js/files-index.js archives
 node assets/js/files-index.js src
 echo ✅ nav indices created!
 echo
+echo ⚙️ building search index...
+rm assets/search/src-search.json
+rm assets/search/archives-search.json
+node assets/js/search-index.js src
+node assets/js/search-index.js archives
+echo 
 node assets/js/checksum.js
 echo ✅ checksums/db version updated
 echo
