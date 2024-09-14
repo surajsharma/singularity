6aa6a9c552a1d31566f3cd758fd88ea87630cf4d
surajsharma <suraj@evenzero.in>
2024-09-14 23:14:54 +0530

    fix: emoji checksum

--- assets/js/checksum.js
+++ assets/js/checksum.js
@@ -1,36 +1,31 @@
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import { str } from "crc-32/crc32c";
-
-function readFileAsStringSync(filePath, encoding = 'utf8') {
-    //using async throws lunr off
-    try {
-        const data = readFileSync(filePath, encoding);
-        return JSON.stringify(data);
-    } catch (err) {
-        throw err;
-    }
-}
+import { readFileAsStringSync } from "./utils.js";
 
 function upgradeDbChecksums() {
     try {
         const src = readFileAsStringSync("assets/search/src-search.json");
         const arc = readFileAsStringSync("assets/search/archives-search.json");
+        const emo = readFileAsStringSync("assets/search/emoji-search.json")
 
         const new_schecksum = str(src, 0);
         const new_achecksum = str(arc, 0);
+        const new_echecksum = str(emo, 0);
 
         let dbfile = readFileSync("assets/search/db.json", "utf-8");
 
-        let { db, version, achecksum, schecksum } = JSON.parse(dbfile);
+        let { db, version, achecksum, schecksum, echecksum } = JSON.parse(dbfile);
 
-        if (new_achecksum != achecksum || new_schecksum != schecksum) {
+        if (new_achecksum != achecksum || new_schecksum != schecksum || new_echecksum != echecksum) {
             version = version + 1;
             achecksum = new_achecksum;
             schecksum = new_schecksum;
+            echecksum = new_echecksum;
+
             const filePath = join('assets', 'search', `db.json`);
 
-            writeFileSync(filePath, JSON.stringify({ db, version, achecksum, schecksum }), function (err) {
+            writeFileSync(filePath, JSON.stringify({ db, version, achecksum, schecksum, echecksum }), function (err) {
                 if (err) throw err;
             });
         } else return;
