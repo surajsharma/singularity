4a2b94cdb4bdf231f4d01b1bd09965d9454ef74b
surajsharma <suraj@evenzero.in>
2025-06-10 13:31:53 +0530

    10.06.25

--- scripts/pre-commit-hook.sh
+++ scripts/pre-commit-hook.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 npm run build &&
+
 ./scripts/build-nav-indices.sh &&
-./scripts/build-search-indices.sh &&  
-./scripts/update-checksums.sh
+
+#./scripts/build-search-indices.sh &&  
+#./scripts/update-checksums.sh
